38c9de00354da409e9934bb5ba97e732239ade21
Toni Hukka <berendin@gmail.com>
2024-03-10 20:00:33 +0200

    testing change to action

--- src/ErrorMessage.jsx
+++ src/ErrorMessage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const ErrorMessage = ({ error }) => (
-  <div data-testid="error">An error occured oh no: {error.toString()}</div>
+  <div data-testid="error">An error occured oh noo: {error.toString()}</div>
 )
 
 export default ErrorMessage
